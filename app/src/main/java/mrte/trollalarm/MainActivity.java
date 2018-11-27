84629e407fcf72f7349117877aa1d254ab877cfa
Christian McGovern <mcgovernchristian@gmail.com>
2018-11-26 20:05:28 -0700

    added new files for alarm clock

--- app/src/main/java/mrte/trollalarm/MainActivity.java
+++ app/src/main/java/mrte/trollalarm/MainActivity.java
@@ -1,36 +1,77 @@
 package mrte.trollalarm;
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSet;
-    EditText etVal;
+
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static MainActivity inst;
+    private TextView alarmTextView;
+    Calendar calendar = null;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSet = (Button)findViewById(R.id.btnSetAlarm);
-        etVal = (EditText)findViewById(R.id.etVal);
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
 
-        btnSet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int time = Integer.parseInt(etVal.getText().toString());
-                Intent intent=new Intent(MainActivity.this, Alarm.class);
-                PendingIntent p1=PendingIntent.getBroadcast(getApplicationContext(),0, intent,0);
-                AlarmManager a=(AlarmManager)getSystemService(ALARM_SERVICE);
-                a.set(AlarmManager.RTC,System.currentTimeMillis() + time*1000,p1);
-                //Toast.makeText(getApplicationContext(),"Alarm set in "+time+"seconds",Toast.LENGTH_LONG).show();
+    }
 
-            }
-        });
+    @TargetApi(Build.VERSION_CODES.M)
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MainActivity", "Alarm On");
+
+            Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(getBaseContext(), 0, myIntent, 0);
+
+            calendar = Calendar.getInstance();
+
+
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+
+
+            alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MainActivity", "Alarm Off");
+        }
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
     }
 
 }
